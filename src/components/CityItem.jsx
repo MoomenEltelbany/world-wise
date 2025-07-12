b4d42f945879243c13e547214f2f7b29e6d054ab
Moomen Eltelbany <73857848+MoomenEltelbany@users.noreply.github.com>
2025-07-12 13:54:02 +0300

    Init the Project starting from the Map Component

---
+++ src/components/CityItem.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+
+import { useCities } from "../contexts/CitiesContext";
+
+import styles from "./CityItem.module.css";
+
+const formatDate = (date) =>
+    new Intl.DateTimeFormat("en", {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+    }).format(new Date(date));
+
+function CityItem({ city }) {
+    const { cityName, date, emoji, position } = city;
+
+    const { currentCity } = useCities();
+
+    const { lat, lng } = position;
+
+    return (
+        <li>
+            <Link
+                to={`${city.id}?lat=${lat}&lng=${lng}`}
+                className={`${styles.cityItem} ${
+                    currentCity.id === city.id ? styles["cityItem--active"] : ""
+                }`}
+            >
+                <span className={styles.emoji}>{emoji}</span>
+                <h3 className={styles.name}>{cityName}</h3>
+                <time className={styles.date}>{formatDate(date)}</time>
+                <button className={styles.deleteBtn}>x</button>
+            </Link>
+        </li>
+    );
+}
+
+export default CityItem;
