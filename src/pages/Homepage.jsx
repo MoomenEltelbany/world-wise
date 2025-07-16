c64c5cb6c779fceebac5002874fb378c4257f553
Moomen Eltelbany <73857848+MoomenEltelbany@users.noreply.github.com>
2025-07-16 20:38:31 +0300

    Add the ProtectedRoute component

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,15 +1,8 @@
 import { NavLink } from "react-router-dom";
 import PageNav from "../components/PageNav";
 import styles from "./Homepage.module.css";
-import { useAuth } from "../contexts/FakeAuthContext";
 
 export default function Homepage() {
-    const { isAuthenticated } = useAuth();
-
-    console.log(isAuthenticated);
-
-    const linkDestination = isAuthenticated ? "/app" : "/login";
-
     return (
         <main className={styles.homepage}>
             <PageNav />
@@ -24,7 +17,7 @@ export default function Homepage() {
                     can think of. Never forget your wonderful experiences, and
                     show your friends how you have wandered the world.
                 </h2>
-                <NavLink to={linkDestination} className="cta">
+                <NavLink to="login" className="cta">
                     Start Tracking now
                 </NavLink>
             </section>
